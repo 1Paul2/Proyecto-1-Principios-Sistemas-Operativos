dc939297a854d83e34a3290116efead1881a9a98
1Paul2 <pollv3803@gmail.com>
2026-09-08 17:01:24 -0600

    Crear estructura de paquetes del proyecto (hardware, modelo, util, gui)

---
+++ src/minipc/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package minipc;
+
+/**
+ *
+ * @author elenanito
+ */
+public class Main {
+    
+    public static void main (String[] arg){
+        System.out.println("Hola mundo");
+    }
+}
